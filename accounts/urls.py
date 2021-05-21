41b426b3b3a6cab13689dfe250132248d411cdc5
= <=>
2021-05-21 22:13:39 +0600

    registration endpoint auth problem fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,13 @@ from django.urls import path
 from knox import views as knox_views
 
 urlpatterns = [
-    path('', AccountOverview.as_view(), name='account-overview'),
-    path('<str:phone>/', SingleAccount.as_view(), name='account-name'),
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    # '' meansa /accounts/
+    path('', AccountOverview.as_view(), name='account-overview'),
+    #  means /accounts/phone
+    path('<str:phone>/', SingleAccount.as_view(), name='account-name'),
+
 ]
