02d94dcc94befd1af5e9d448d9c045ce0ee92a20
= <=>
2021-05-22 12:22:27 +0600

    updated settings

--- matir_bank/wsgi.py
+++ matir_bank/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebanking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matir_bank.settings')
 
 application = get_wsgi_application()
